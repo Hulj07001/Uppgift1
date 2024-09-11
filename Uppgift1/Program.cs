5cf147a17be8d6d04da9963e0ea8f3408afdd1bb
hulj07001 <hulj07001@E-PW05CGR7>
2024-09-11 12:29:56 +0200

    Add project files.

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Upgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva tecken som \" och \\ ");
+        }
+    }
+}
